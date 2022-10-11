210d81f04b28907520d10e69f5b9ce04a64e9e0c
Grayson Harvey <graycodes.nu@gmail.com>
2022-10-11 17:09:54 -0500

    post route. restructured routes for db json data call. its working!

--- Develop/server.js
+++ Develop/server.js
@@ -13,25 +13,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Call the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 // GET /notes page
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // GET data from db file to append 
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
   const parseNotes = JSON.parse(dataNotes);
   res.json(parseNotes);
 });
 
+// POST note to db and append to page
+app.post('/api/notes', (req, res) => {
+  const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+  const parseNotes = JSON.parse(dataNotes);
+  req.body.id = uuidv1()
+  parseNotes.push(req.body);
 
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
+  res.json("Your note has been added.");
+});
 
+// GET the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
